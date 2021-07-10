259bea738b832ac68093729edbbe03b552f79ce6
Joshua Maney <joshuaraymaney@gmail.com>
2021-07-09 23:05:34 -0400

    Revert "Updates"
    This reverts commit 4fc888dd22119d0a160c335d976d402b3702bb4b.

---
+++ README.md
@@ -0,0 +1,1 @@
+# new_employee_directory
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from "./components/Header/header.js"
+import Table from "./components/Table/table";
+import React from 'react';
+
+
+function App() {
+  return (
+      <div>
+        <Header />
+        <Table />
+      </div>
+  );
+}
+
+export default App;
